030e81a7d165a256359b28fa161e238aa46ae7d3
duteoj <yusupasylzhan2006@gmail.com>
2025-12-09 12:37:56 +0500

    141

--- Bank.java
+++ Bank.java
@@ -3,12 +3,21 @@ public class Bank {
     private String name;
     private ArrayList<Account> accounts = new ArrayList<>();
 
+    public Bank(String Name){
+        this.name = Name;
+
+    }
+
     public String getName() {
         return name;
     }
 
     public void setName(String name){
         this.name = name;
+    }
+
+    publci void AddAccount(Account account){
+        accounts.add(account);
     }
 
 //suii
